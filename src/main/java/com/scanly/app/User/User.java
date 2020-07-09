d33e1dde9c7fa828522e7640f5108250f5a92007
Halahaddad1 <h.m.haddad@gmail.com>
2020-07-09 14:08:07 -0700

    server running now

--- src/main/java/com/scanly/app/User/User.java
+++ src/main/java/com/scanly/app/User/User.java
@@ -1,16 +1,11 @@
 package com.scanly.app.User;
 
-import jdk.jfr.DataAmount;
-import jdk.jfr.events.CertificateId;
+
 import lombok.Data;
 
 public class User {
 
-    @Data
-    class User{
 
-        @Id
-        @GeneratedValue(strategy=GenerationType.AUTO)
         private Long id;
         private String name;
 
@@ -21,4 +16,4 @@ public class User {
             this.name = name;
         }
     }
-}
+

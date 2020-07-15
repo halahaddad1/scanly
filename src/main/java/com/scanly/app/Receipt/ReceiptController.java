24710f926578a91ca457137830907e908c733aa9
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-07-14 23:14:04 -0700

    can call ocrImage method of server from postman

--- src/main/java/com/scanly/app/Receipt/ReceiptController.java
+++ src/main/java/com/scanly/app/Receipt/ReceiptController.java
@@ -15,36 +15,31 @@ public class ReceiptController {
     @Autowired
     FirebaseService firebaseService;
 
-    @PostMapping(value = "/ocrImage")
+    @PostMapping("/ocrImage")
     public void klippaImage(@RequestParam("file") MultipartFile file) throws IOException {
         QuoteOfTheDay quote = new QuoteOfTheDay();
         quote.klippaMultiPartPostRequest(file.getBytes());
-
-        // passing the filepath to the service method
-//        Object ocrDataImage = firebaseService.klippaImage(fileName);
-//        return Response.ResponseBuilder.Build();
-
     }
 
 
-        @GetMapping("/getReceiptDetails")
-        public Receipt getReceipt(@RequestParam(required = false) String name) throws InterruptedException, ExecutionException {
-            return firebaseService.getReceiptDetails(name);
-        }
-
-        @PostMapping("/createReceipt")
-        public String createReceipt(@RequestBody Receipt receipt) throws InterruptedException, ExecutionException {
-            return firebaseService.saveReceiptDetails(receipt);
-        }
-
-        @PutMapping("/updateReceipt")
-        public String updateReceipt(@RequestBody Receipt receipt ) throws InterruptedException, ExecutionException {
-            return firebaseService.updateReceiptDetails(receipt);
-        }
-
-        @DeleteMapping("/deleteReceipt")
-        public String deleteReceipt(@RequestParam String name){
-            return firebaseService.deleteReceipt(name);
-        }
+    @GetMapping("/getReceiptDetails")
+    public Receipt getReceipt(@RequestParam(required = false) String name) throws InterruptedException, ExecutionException {
+        return firebaseService.getReceiptDetails(name);
     }
+
+    @PostMapping("/createReceipt")
+    public String createReceipt(@RequestBody Receipt receipt) throws InterruptedException, ExecutionException {
+        return firebaseService.saveReceiptDetails(receipt);
+    }
+
+    @PutMapping("/updateReceipt")
+    public String updateReceipt(@RequestBody Receipt receipt ) throws InterruptedException, ExecutionException {
+        return firebaseService.updateReceiptDetails(receipt);
+    }
+
+    @DeleteMapping("/deleteReceipt")
+    public String deleteReceipt(@RequestParam String name){
+        return firebaseService.deleteReceipt(name);
+    }
+}
 
